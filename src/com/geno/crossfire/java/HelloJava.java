4047f19e11a3bb21b55f984bfa4037e8fb906d9b
Geno1024 <754097987@qq.com>
2018-01-20 18:32:37 +0800

    [Update] Add Java.

---
+++ src/com/geno/crossfire/java/HelloJava.java
@@ -0,0 +1,14 @@
+package com.geno.crossfire.java;
+
+public class HelloJava
+{
+    public static void hello()
+    {
+        System.out.println("Hello Java!");
+    }
+
+    public static void main(String[] args)
+    {
+        HelloJava.hello();
+    }
+}
